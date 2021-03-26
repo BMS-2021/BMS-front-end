332dbbb088807f54177559054c9ee0ddd182890f
enzymii <enzymiix@gmail.com>
2021-03-26 12:02:26 +0800

    create next app with typescript and eslint

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/react-in-jsx-scope */
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/api/hello.tsx
@@ -0,0 +1,6 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export default (req, res): void => {
+  res.status(200).json({ name: 'John Doe' });
+};
