c4fcf24a464716a70e4b1ad71ae64cc804dc60f7
enzymii <enzymiix@gmail.com>
2021-04-09 00:16:04 +0800

    Finish import single book interface

--- Components/Contents/ImportBook.tsx
+++ Components/Contents/ImportBook.tsx
@@ -2,16 +2,28 @@ import React, { ReactElement } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { useForm } from 'react-hook-form';
 import neofetch from '../../utils/neofetch';
-import { Snackbar, TextField, Button } from '@material-ui/core';
+import { Snackbar, TextField, Button, Container } from '@material-ui/core';
 import { useImmer } from 'use-immer';
 
 const useStyles = makeStyles((theme) => ({
+  container: {
+    backgroundColor: '#ffffff',
+    borderRadius: theme.spacing(3),
+    padding: theme.spacing(4, 0, 4),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    boxShadow: theme.shadows[6],
+  },
   form: {
-    width: '100%',
-    marginTop: theme.spacing(1),
+    width: '70%',
+    margin: theme.spacing(3, 0, 1),
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    marginTop: theme.spacing(5),
+    marginLeft: '40%',
+    width: '20%',
+    fontSize: theme.typography.fontSize * 1.1,
   },
 }));
 
@@ -36,7 +48,7 @@ export default function ImportBook(): ReactElement {
     { name: 'category', label: '类别', type: 'text' },
     { name: 'press', label: '出版社', type: 'text' },
     { name: 'year', label: '年份', type: 'number' },
-    { name: 'price', label: '价格', type: 'text', }, // TODO: check decimal
+    { name: 'price', label: '价格', type: 'text', pattern: /^\d+(\.\d+\d?)?$/ }, // TODO: check decimal
     { name: 'total', label: '总量', type: 'number' },
     { name: 'stock', label: '库存', type: 'number' },
   ];
@@ -71,7 +83,7 @@ export default function ImportBook(): ReactElement {
     }
   };
 
-  const formFields = formParams.map(({ name, label, ...rest }) => (
+  const formFields = formParams.map(({ name, label, pattern, ...rest }) => (
     <TextField
       key={name}
       variant='standard'
@@ -83,9 +95,10 @@ export default function ImportBook(): ReactElement {
       name={name}
       inputRef={register({
         required: true,
+        pattern,
       })}
       error={Boolean(inputError[name])}
-      helperText={inputError[name] ? `${label}不能为空！` : null}
+      helperText={inputError[name] ? `请输入正确的${label}！` : null}
       {...rest}
     />
   ));
@@ -104,17 +117,19 @@ export default function ImportBook(): ReactElement {
         message={snackState.message}
         key={snackState.message}
       ></Snackbar>
-      <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
-        {formFields}
-        <Button
-          type='submit'
-          variant='contained'
-          color='primary'
-          className={classes.submit}
-        >
-          导入
-        </Button>
-      </form>
+      <Container maxWidth='sm' className={classes.container}>
+        <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+          {formFields}
+          <Button
+            type='submit'
+            variant='contained'
+            color='primary'
+            className={classes.submit}
+          >
+            导入
+          </Button>
+        </form>
+      </Container>
     </>
   );
 }
